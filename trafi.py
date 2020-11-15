d72997fab309bf6deef6a40d6fff5a24bf6764eb
harcsabalu <64604783+harcsabalu@users.noreply.github.com>
2020-11-15 21:27:27 +0100

    Add files via upload

--- trafi.py
+++ trafi.py
@@ -3,7 +3,26 @@ import xlrd
 import sys
 
 dataList = []
+speedLimits = {
+    'sz': 130,
+    'm': 130,
+    'b': 100,
+    't': 80,
+    'mk': 1000 # "nincs korlát"
+}
+def elsoFeladat():
+    speeders = 0
+    for i,v in enumerate(dataList):
+        felsegjel,rendszam,merohely,jarmutipus,sebesseg,ido = convToStrings(v)
+        if merohely == 'A' and jarmutipus == 'm':
+            if int(sebesseg) > speedLimits[jarmutipus]:
+                speeders += 1
+    print(speeders)
+    masodikFeladat()
 
+def masodikFeladat():
+    print("aa")
+    
 def convToList(fileType, Thefile):
     if fileType == 1:
         for line in Thefile:
@@ -11,8 +30,12 @@ def convToList(fileType, Thefile):
     elif fileType == 2:
         for x in range(Thefile.nrows):
             dataList.append(Thefile.cell_value(x,0))
-    print(dataList)
+    elsoFeladat()
+    print(dataList)           
 
+def convToStrings(data):
+    strings = data.split(",", -1)
+    return strings
 
 def choose():
     print("Kérem ajda meg a fájl elérési útvonalát: ")
@@ -33,4 +56,3 @@ def choose():
 
 choose()
 
-        
